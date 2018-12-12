fbdd4f9418c25ae1216ac6e8842b5d6ee66c8fb3
clywzach <31866139+clywzach@users.noreply.github.com>
2018-12-12 16:43:56 -0700

    rename/formatting

--- proj5/OrderedSet.py
+++ proj5/OrderedSet.py
@@ -5,11 +5,11 @@ class OrderedSet(collections.Set):
     def __init__(self, iterable=()):
         self.d = collections.OrderedDict.fromkeys(iterable)
 
-    def __len__(self):
-        return len(self.d)
-
-    def __contains__(self, element):
-        return element in self.d
-
     def __iter__(self):
         return iter(self.d)
+
+    def __contains__(self, el):
+        return el in self.d
+
+    def __len__(self):
+        return len(self.d)
